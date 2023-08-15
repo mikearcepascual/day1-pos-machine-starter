9cff68ccdb767decb6ae43c2407c1b3ca9f22f26
MIKEARCE PASCUAL (PLAN-IRIS-ISD-OOCLL/MNL) <PASCUMI3@OOCL.COM>
2023-08-15 21:06:41 +0800

    chore: added ReceiptItem object class

---
+++ src/main/java/pos/machine/ReceiptItem.java
@@ -0,0 +1,27 @@
+package pos.machine;
+
+public class ReceiptItem {
+    private String name;
+    private int quantity;
+    private int unitPrice;
+
+    private int subTotal;
+
+    public ReceiptItem(String name, int quantity, int unitPrice, int subTotal) {
+        this.quantity = quantity;
+        this.name = name;
+        this.unitPrice = unitPrice;
+        this.subTotal = subTotal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getUnitPrice() { return unitPrice; }
+
+    public int getQuantity() { return quantity; }
+
+    public int getSubTotal(){ return subTotal; }
+
+}
